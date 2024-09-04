850e33e9362c283ba5e30048d29e6058bcbfca20
Nghia <19204686+nghiamphan@users.noreply.github.com>
2024-09-04 13:27:11 -0300

    when click finish test, the recorded audio files will be saved to backend's file system

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,25 @@ import { Box, Button, Container, List, ListItemButton, Typography } from '@mui/m
 import { useEffect, useState } from 'react'
 import { useReactMediaRecorder } from 'react-media-recorder'
 
-import { getAudio } from './services/audio'
+import { getAudio, saveAudios } from './services/audio'
 
 const App = () => {
     const [displayTest, setDisplayTest] = useState(false)
     const [questionNo, setQuestionNo] = useState(1)
     const [audioUrl, setAudioUrl] = useState(null)
+    const [recordAudioUrls, setRecordAudioUrls] = useState({
+        answer_1: null,
+        answer_2: null,
+        answer_3: null,
+        answer_4: null,
+        answer_5: null,
+    })
     const [isNewQuestion, setIsNewQuestion] = useState(true)
     const [isRecording, setIsRecording] = useState(false)
     const { startRecording, stopRecording, mediaBlobUrl } = useReactMediaRecorder({ audio: true })
 
     const questions = ['/k/ + /aw/', '/t/ + /o_e/', '/t/ + /a_e/', '/sh/ + /oo/', '/f/ + /ee/']
-    const audio_filepaths = [
+    const questionAudioFilepaths = [
         'questions/analysis/question_k_aw.mp3',
         'questions/analysis/question_t_oe.mp3',
         'questions/analysis/question_t_oe.mp3',
@@ -21,13 +28,35 @@ const App = () => {
         'questions/analysis/question_t_oe.mp3',
     ]
 
+    const answerAudioFilepaths = [
+        'tests/2024-09-03/test_id/answer_1.mp3',
+        'tests/2024-09-03/test_id/answer_2.mp3',
+        'tests/2024-09-03/test_id/answer_3.mp3',
+        'tests/2024-09-03/test_id/answer_4.mp3',
+        'tests/2024-09-03/test_id/answer_5.mp3',
+    ]
+
     useEffect(() => {
         const fetchAudio = async () => {
-            const fetched_audio = await getAudio(audio_filepaths[0])
+            const fetched_audio = await getAudio(questionAudioFilepaths[0])
             setAudioUrl(fetched_audio)
         }
         fetchAudio()
     }, [])
+
+    useEffect(() => {
+        if (mediaBlobUrl) {
+            setRecordAudioUrls({ ...recordAudioUrls, ['answer_' + questionNo]: mediaBlobUrl })
+        }
+
+        // State hook is asynchronous, so the updated state of recordAudioUrls may not be available immediately in the console.log statement below
+        console.log(
+            'useEffect when mediaBlobUrl is updated; Recorded Audio URLs: ',
+            recordAudioUrls,
+            'last recorded audio URL: ',
+            mediaBlobUrl
+        )
+    }, [mediaBlobUrl])
 
     const TestList = () => {
         return (
@@ -55,9 +84,31 @@ const App = () => {
     }
 
     const onClickNextQuestion = async () => {
+        setIsNewQuestion(true)
         setQuestionNo(questionNo + 1)
-        const fetched_audio = await getAudio(audio_filepaths[questionNo])
+        const fetched_audio = await getAudio(questionAudioFilepaths[questionNo])
         setAudioUrl(fetched_audio)
+
+        console.log(
+            'onClickNextQuestion; Recorded Audio URLs: ',
+            recordAudioUrls,
+            'last recorded audio URL: ',
+            mediaBlobUrl
+        )
+    }
+
+    const onClickFinishQuestion = () => {
+        setDisplayTest(false)
+        setQuestionNo(1)
+
+        saveAudios(recordAudioUrls, answerAudioFilepaths)
+
+        console.log(
+            'onClickFinishQuestion; Recorded Audio URLs: ',
+            recordAudioUrls,
+            'last recorded audio URL: ',
+            mediaBlobUrl
+        )
     }
 
     const Test = () => {
@@ -70,8 +121,7 @@ const App = () => {
                 </Typography>
                 <Typography variant="h4">Question {questionNo} </Typography>
                 <Typography>{questions[questionNo - 1]}</Typography>
-                <audio controls>
-                    <source src={audioUrl} type="audio/mp3" />
+                <audio controls src={audioUrl} type="audio/mp3">
                     Your browser does not support the audio element.
                 </audio>
                 <Box>
@@ -83,20 +133,13 @@ const App = () => {
                         )}
                     </Box>
                     {!isRecording && mediaBlobUrl && !isNewQuestion && (
-                        <audio controls src={mediaBlobUrl}></audio>
+                        <audio controls src={mediaBlobUrl} />
                     )}
                 </Box>
                 {questionNo < questions.length ? (
                     <Button onClick={onClickNextQuestion}>Next Question</Button>
                 ) : (
-                    <Button
-                        onClick={() => {
-                            setDisplayTest(false)
-                            setQuestionNo(1)
-                        }}
-                    >
-                        Finish Test
-                    </Button>
+                    <Button onClick={onClickFinishQuestion}>Finish Test</Button>
                 )}
             </Box>
         )
--- src/services/audio.js
+++ src/services/audio.js
@@ -1,16 +1,57 @@
 import axios from 'axios'
 
-const backendUrl = 'http://localhost:8000/api/audio'
+const backendUrl = 'http://localhost:8000/api/audio/'
 
 /**
  * Get audio file from backend server by filepath
- * @param {string} filepath
+ * @param {string} relativeAudioFilepath - the filepath of the audio file in the file system in the backend server
  * @returns {any} response.data
  */
-const getAudio = async (filepath) => {
-    const response = await axios.get(`${backendUrl}/${filepath}`, { responseType: 'blob' })
+const getAudio = async (relativeAudioFilepath) => {
+    const response = await axios.get(`${backendUrl}${relativeAudioFilepath}`, { responseType: 'blob' })
     const audioUrl = URL.createObjectURL(response.data)
     return audioUrl
 }
 
-export { getAudio }
+/**
+ * Save audio files to backend server
+ *
+ * The request body will be like this:
+ * {
+ *    "answer_0": Blob,
+ *    "answer_1": Blob,
+ *    "answer_2": Blob,
+ *    ...,
+ *    "relative_answer_filepaths": '["tests/2024-09-03/<test_id>/answer_<test_question_id>.mp3",  ...]'
+ * }
+ *
+ * @param {Object<string, string>} audioUrls - the list of objects like this: { 'answer_0': audio_url1, 'answer_1': audio_url2, ... }
+ * @param {string[]} relativeAudioFilepaths - the relative filepaths in the file system in the backend server to save the audio files
+ * @returns {any} response
+ */
+const saveAudios = async (audioUrls, relativeAudioFilepaths) => {
+    const formData = new FormData()
+
+    // Convert each audio URL to a Blob and append it to the FormData object with its corresponding key
+    await Promise.all(
+        Object.entries(audioUrls).map(async ([key, audioUrl]) => {
+            if (!audioUrl) {
+                formData.append(key, null)
+            } else {
+                const response = await axios.get(audioUrl, { responseType: 'blob' })
+                formData.append(key, response.data)
+            }
+        })
+    )
+    // Serialize the list of filepaths into a JSON string
+    const filepathsJson = JSON.stringify(relativeAudioFilepaths)
+
+    // Append the JSON string of filepaths to the FormData object
+    formData.append('relative_audio_filepaths', filepathsJson)
+
+    // Send the FormData object in a POST request to the backend server
+    const response = await axios.post(backendUrl, formData)
+    return response
+}
+
+export { getAudio, saveAudios }
