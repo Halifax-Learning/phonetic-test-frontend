a40122231d5b6ccad4fd25fa1dcb40e19013eef9
Ka Man Leung <kaman.leung@smu.ca>
2024-09-20 13:47:38 -0300

    Update return button functionality to navigate to homepage on AssessmentFinish

--- src/components/assessment/AssessmentFinish.tsx
+++ src/components/assessment/AssessmentFinish.tsx
@@ -1,13 +1,12 @@
 import HeaderIcon from '@mui/icons-material/DoneAll'
 import { Box, Button, Card, CardContent, Grid2, Typography } from '@mui/material'
-import { useDispatch } from 'react-redux'
-import { setScreenToDisplay } from '../../reducers/screenToDisplayReducer'
+import { useNavigate } from 'react-router-dom'
 
 const AssessmentFinish = () => {
-    const dispatch = useDispatch<any>()
+    const navigate = useNavigate()
 
-    const returnToAssessmentList = () => {
-        dispatch(setScreenToDisplay('AssessmentList'))
+    const returnToHomeScreen = () => {
+        navigate('/')
     }
     return (
         <Box
@@ -54,7 +53,7 @@ const AssessmentFinish = () => {
                                 <Button
                                     variant="contained"
                                     sx={{ padding: '12px' }}
-                                    onClick={returnToAssessmentList}
+                                    onClick={returnToHomeScreen}
                                 >
                                     Return to Home Screen
                                 </Button>
